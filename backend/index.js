aed56a48e666cad1be10fb24e1781672049f83a5
TSdnl13 <daniel.int@outlook.com>
2022-05-17 22:16:28 -0500

    add: mongoose connection and app use posts route

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,25 @@ import express from 'express';
 import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONECTION_URL = process.env.EXPRESS_CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(CONECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
+   .catch(error => console.log(error.message));
