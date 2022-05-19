75f1264c59678616086719a541d6b63b8ef4cfaf
TSdnl13 <daniel.int@outlook.com>
2022-05-18 22:31:36 -0500

    update: move /posts route and delete no use

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -11,11 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONECTION_URL = process.env.EXPRESS_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
