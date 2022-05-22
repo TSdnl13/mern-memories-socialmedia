ba8876bb4e78e6d0923a36cf0ca8407b9efb03c3
TSdnl13 <daniel.int@outlook.com>
2022-05-21 22:12:43 -0500

    add: functions related to likeCount of a post

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
    const classes = useStyles();
@@ -39,22 +39,20 @@ const Post = ({ post, setCurrentId }) => {
                {post.title}
          </Typography>
          <CardContent>
-            <Typography variant='h6' gutterBottom>
+            <Typography variant='body2' color='textSecondary' component='p'>
                {post.message}
             </Typography>
          </CardContent>
 
          <CardActions className={classes.cardActions}>
-            <Button size='small' color='primary' onClick={() => {}}>
+            <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                <ThumbUpAltIcon fontSize='small' />
-               Like
-               {post.lokeCount}
+               Like {post.likeCount}
             </Button>
 
             <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
                <DeleteIcon fontSize='small' />
                Delete
-               {post.lokeCount}
             </Button>
          </CardActions>
       </Card>
