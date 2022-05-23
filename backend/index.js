56c936de06d144f1e0f92d91b32499cec7af3098
TSdnl13 <daniel.int@outlook.com>
2022-05-23 17:26:49 -0500

    some fixes for deploying in heroku

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,16 @@ dotenv.config();
 
 const app = express();
 
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+   res.send('Hello to memories API');
+});
 
 const CONECTION_URL = process.env.EXPRESS_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
