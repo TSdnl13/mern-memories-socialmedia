326f47382932d211eb957c90e782fb9e7b11888f
TSdnl13 <daniel.int@outlook.com>
2022-05-24 15:00:07 -0500

    add: logout functionality and useEffect for update the user

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
    const classes = useStyles();
+   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   const location = useLocation();
 
-   const user = null;
-    
+   useEffect(() => {
+      const token = user?.token;
+
+      setUser(JSON.parse(localStorage.getItem('profile')));
+   }, [location]);
+   
+   const logout = () => {
+      dispatch({ type: 'LOGOUT' });
+      navigate('/');
+      setUser(null);
+   }
+
    return (
       <AppBar className={classes.appBar} position='static' color='inherit'>
          <div className={classes.brandContainer}>
@@ -36,7 +51,12 @@ const Navbar = () => {
                      {user.result.name}
                   </Typography>
 
-                  <Button variant='contained' className={classes.logout} color='secondary'>
+                  <Button 
+                     variant='contained' 
+                     className={classes.logout} 
+                     color='secondary'
+                     onClick={logout}
+                  >
                      Logout
                   </Button>
                </div>
