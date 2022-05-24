902c9729a1c7dc000a6b5cebfdf64ac33ab17a51
TSdnl13 <daniel.int@outlook.com>
2022-05-24 16:55:46 -0500

    add: /user router to app

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,18 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 dotenv.config();
 
 const app = express();
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
    res.send('Hello to memories API');
