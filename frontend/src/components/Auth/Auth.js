11483a0d60191dc8389ecd3a83c9d02fd9ca35b2
TSdnl13 <daniel.int@outlook.com>
2022-05-24 16:54:44 -0500

    add: handleSubmit and change inputs, auth actions created

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -8,22 +8,32 @@ import { useNavigate } from 'react-router-dom';
 import Icon from './Icon';
 import useStyles from './styles';
 import Input from './Input';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: ''};
 
 const Auth = () => {
    const classes = useStyles();
    const [showPassword, setShowPassword] = useState(false);
    const [isSignUp, setIsSignUp] = useState(false);
+   const [formData, setFormData] = useState(initialState);
    const dispatch = useDispatch();
    const navigate = useNavigate();
 
    const handleShowPassword = () => setShowPassword(prevShowPassword => !prevShowPassword);
 
-   const handleSubmit = () => {
+   const handleSubmit = (e) => {
+      e.preventDefault();
 
+      if (isSignUp) {
+         dispatch(signup(formData, navigate));
+      } else {
+         dispatch(signin(formData, navigate));
+      }
    }
 
-   const handleChange = () => {
-
+   const handleChange = (e) => {
+      setFormData({...formData, [e.target.name]: e.target.value })
    }
 
    const switchMode = () => {
