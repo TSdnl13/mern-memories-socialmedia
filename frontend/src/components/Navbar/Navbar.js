6d4e498847d386fa1446424d9565be6d832cf5b7
TSdnl13 <daniel.int@outlook.com>
2022-05-26 18:57:36 -0500

    add: fix useEffect token

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import decode from 'jwt-decode';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
@@ -15,13 +16,17 @@ const Navbar = () => {
 
    useEffect(() => {
       const token = user?.token;
+      if (token) {
+         const decodedToken = decode(token);
 
+         if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+      }
       setUser(JSON.parse(localStorage.getItem('profile')));
    }, [location]);
    
    const logout = () => {
       dispatch({ type: 'LOGOUT' });
-      navigate('/');
+      navigate('/auth');
       setUser(null);
    }
 
