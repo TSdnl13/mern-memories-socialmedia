26dc71b2e4d31fb6e5eecc4f06887cac259bba32
TSdnl13 <daniel.int@outlook.com>
2022-05-26 19:01:50 -0500

    change currentId from default null to 0

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -8,7 +8,7 @@ import Posts from '../Posts/Posts';
 import useStyles from './styles';
 
 const Home = () => {
-   const [currentId, setCurrentId] = useState(null);
+   const [currentId, setCurrentId] = useState(0);
    const dispatch = useDispatch();
    const classes = useStyles();
    
