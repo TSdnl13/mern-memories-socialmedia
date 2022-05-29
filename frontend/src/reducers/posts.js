fd201e365fcca0c9532f724b8eaeefbb3dd764b9
TSdnl13 <daniel.int@outlook.com>
2022-05-29 17:54:46 -0500

    some fixes change state from array to object

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from "../constants/actionTypes"; 
 
-const posts =  (state = [], action) => {
+const posts =  (state = { isLoading: true, posts: [] }, action) => {
    switch (action.type) {
       case START_LOADING:
          return { ...state, isLoading: true }
@@ -16,11 +16,12 @@ const posts =  (state = [], action) => {
       case FETCH_BY_SEARCH:
          return { ...state, posts: action.payload }
       case CREATE:
-         return [...state, action.payload];
+         return { ...state, posts: [...state.posts, action.payload] }
       case UPDATE: 
-         return state.map(post => post._id === action.payload._id ? action.payload : post);
+         return { ...state, 
+            posts: state.posts.map(post => post._id === action.payload._id ? action.payload : post) }
       case DELETE: 
-         return state.filter(post => post._id !== action.payload);
+         return { ...state, posts: state.posts.filter(post => post._id !== action.payload) }
       default:
          return state;
    }
